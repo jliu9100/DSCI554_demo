c1d43964edf86603cdd96d10560f7adff9c0731d
jliu9100 <jliu9100@usc.edu>
2023-11-16 11:43:01 -0800

    update MapView & DataLoader

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,33 +1,59 @@
 <template>
   <div class="map-container">
-    <l-map :zoom="zoom" :min-zoom="zoom - 2" :max-zoom="zoom + 2" :center="center" style="height: 100%;">
+    <l-map :zoom="zoom" :min-zoom="zoom" :max-zoom="zoom + 4" :center="center" style="height: 100%;">
       <l-tile-layer :url="tileUrl" :attribution="attribution" />
-      <l-marker v-for="dataPoint in props.dataPoints" :key="dataPoint.id" :lat-lng="[dataPoint.lat, dataPoint.lng]"
-        @ready="console.log(dataPoint.id, [dataPoint.lat, dataPoint.lng])">
-        <l-icon :icon-size="[32, 37]" :icon-anchor="[16, 37]" icon-url="fire.png">
+      <l-marker v-for="dataPoint in enrichedDataPoints" :key="dataPoint.id" :lat-lng="[dataPoint.lat, dataPoint.lng]"
+      >
+        <l-icon :icon-size="[dataPoint.iconSize,dataPoint.iconSize]"  icon-url="fire.png">
         </l-icon>
+        <l-popup :content="dataPoint.name+' '+dataPoint.date+' '+dataPoint.lat+','+dataPoint.lng">
+        </l-popup>
       </l-marker>
     </l-map>
   </div>
-  {{ [(props.dataPoints[0].lat), props.dataPoints[0].lng] }}
   <div>
 
   </div>
 </template>
   
 <script setup>
-import { ref, defineProps } from 'vue';
-import { LMap, LTileLayer, LMarker, LIcon } from '@vue-leaflet/vue-leaflet';
+import { ref, defineProps, computed, onMounted } from 'vue';
+import { LMap, LTileLayer, LMarker, LIcon, LPopup } from '@vue-leaflet/vue-leaflet';
 import 'leaflet/dist/leaflet.css';
 const props = defineProps({
   dataPoints: Array
 });
+
+const minSize = 20;
+const maxSize = 50;
+const minAcreage = ref(0);
+const maxAcreage = ref(0);
 
 const zoom = ref(5);
 const center = ref([36.7783, -119.4179]);
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const attribution = '&copy; <a target="_blank" href="http://osm.org/copyright">CaliforniaMap</a>';
 
+const updateAcreageBounds = () => {
+  const acreages = props.dataPoints.map(p => p.acreage);
+  minAcreage.value = Math.min(...acreages);
+  maxAcreage.value = Math.max(...acreages);
+};
+
+onMounted(updateAcreageBounds);
+
+const calculateIconSize = (acreage) => {
+  const sizeRatio = (acreage - minAcreage.value) / (maxAcreage.value - minAcreage.value);
+  const size = minSize + (maxSize - minSize) * sizeRatio;
+  return Math.max(minSize, Math.min(size, maxSize));
+};
+
+const enrichedDataPoints = computed(() => 
+  props.dataPoints.map(dp => ({
+    ...dp,
+    iconSize: calculateIconSize(dp.acreage)
+  }))
+);
 
 
 </script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,14 @@
 <!-- src/views/HomePage.vue -->
 <template>
   <DataLoader v-slot="{ dataPoints }">
-    <div>
+    
+    <div v-if="dataPoints && dataPoints.length">
       <h1>{{ title }}</h1>
       <MapView :dataPoints="dataPoints" />
     </div>
+    <div v-else>
+      <h1>Loading...</h1>
+  </div>
   </DataLoader>
 </template>
   
