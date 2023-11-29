2f053002df1d4738fa285cab54a87b7d687db4db
Rickenqi <rickenqi@outlook.com>
2023-11-28 23:32:18 -0800

    nav bar revise

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,18 +1,18 @@
 <template>
   <el-menu
-    :default-active="this.$route.path"
-    router
-    class="el-menu-demo"
-    mode="horizontal"
-    @select="handleSelect"
+      :default-active="this.$route.path"
+      router
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
   >
     <el-menu-item index="/Home">Home Page</el-menu-item>
     <el-menu-item index="/Page1">By County</el-menu-item>
     <el-menu-item index="/Page2">By Cause</el-menu-item>
-    <el-menu-item index="/Page3">Page3</el-menu-item>
-    <el-menu-item index="/Page4">Page4</el-menu-item>
-    <el-menu-item index="/pack">Pack(By Damage)</el-menu-item>
-    <el-menu-item index="/timelapse">Timelapse</el-menu-item>
+    <el-menu-item index="/Page3">Risky Area Prediction</el-menu-item>
+    <!--    <el-menu-item index="/Page4">Page4</el-menu-item>-->
+    <el-menu-item index="/pack">Damage Pack (experimental)</el-menu-item>
+    <el-menu-item index="/timelapse">Timelapse (experimental)</el-menu-item>
   </el-menu>
 </template>
 
