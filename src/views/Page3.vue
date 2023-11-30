1eb596645b50ea8e344059a691f7467ee2cc1f64
Jade Hackett <seanseanhacketthackett@gmail.com>
2023-11-30 04:52:29 -0800

    Use pinia for page 3

--- src/views/Page3.vue
+++ src/views/Page3.vue
@@ -1,22 +1,22 @@
 <!-- src/views/SubPage1.vue -->
 <template>
-  <DataLoader @update:data="handleDataUpdate" />
+  <!-- <DataLoader @update:data="handleDataUpdate" /> -->
   <div v-loading.fullscreen.lock="isLoading1">
     <h1>{{ title }}</h1>
 
 
     <el-row>
       <el-col :span="12">
-        <div v-if="dataPoints && dataPoints.length">
+        <div v-if="data && data.length">
           <h2>{{ subtitle1 }}</h2>
-          <Page3DensityMap :dataPoints="null" mapType="map1" @loading="handleMapLoading" @loaded="handleMapLoaded" />
+          <Page3DensityMap :dataPoints="data" mapType="map1" @loading="handleMapLoading" @loaded="handleMapLoaded" />
         </div>
       </el-col>
 
       <el-col :span="12">
-        <div v-if="dataPoints && dataPoints.length">
+        <div v-if="data && data.length">
           <h2>{{ subtitle2 }}</h2>
-          <Page3DensityMap :dataPoints="null" mapType="map2" @loading="handleMapLoading" @loaded="handleMapLoaded" />
+          <Page3DensityMap :dataPoints="data" mapType="map2" @loading="handleMapLoading" @loaded="handleMapLoaded" />
         </div>
       </el-col>
 
@@ -28,6 +28,9 @@
 import { ref, computed } from 'vue';
 import DataLoader from '@/components/DataLoader.vue';
 import Page3DensityMap from '@/components/Page3DensityMap.vue';
+import { useGeneralDataStore } from "@/stores/generalData";
+
+const { data }  = useGeneralDataStore();
 
 const title = ref('Risky areas for wild fire in California');
 const subtitle1 = ref('Map of risky area for wild fire based on prediction for potential locations');
@@ -35,9 +38,9 @@ const subtitle2 = ref('Map of risky area for wild fire based on year 2015');
 const dataPoints = ref([]);
 const isLoading1 = ref(true);
 
-const handleDataUpdate = (updatedData) => {
-  dataPoints.value = updatedData;
-};
+// const handleDataUpdate = (updatedData) => {
+//   dataPoints.value = updatedData;
+// };
 
 const handleMapLoading = () => {
     isLoading1.value = true;
