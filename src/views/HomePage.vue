93b54eba8a734cb54ccf755dcc489d3932a51ecf
Jade Hackett <seanseanhacketthackett@gmail.com>
2023-11-30 10:24:00 -0800

    Use pinia for main map

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,17 +1,14 @@
 <!-- src/views/HomePage.vue -->
 <template>
+  <!-- <DataLoader @update:data="handleDataUpdate" /> -->
+  <div class="map-container">
+    <h1 class="map-title">{{ title }}</h1>
+    <YearSelector :initial-year="currentYear" :initialMonth="currentMonth" @update:yearMonth="updateYearMonth" />
+    <MapView :currentMonth="currentMonth" :currentYear="currentYear" />
 
-  <DataLoader @update:data="handleDataUpdate" />
-  <div class="map-container" v-loading.fullscreen.lock="isLoading">
-    <div v-if="dataPoints && dataPoints.length">
-      <h1 class="map-title">{{ title }}</h1>
-      <YearSelector :initial-year="currentYear" :initialMonth="currentMonth" @update:yearMonth="updateYearMonth" />
-      <MapView :dataPoints="filteredDataPoints" />
-      
-      <HomePageBarChart :dataPoints="dataPoints" />
-      <TimelapseContainer />
-      <HomePageHeatMap :dataPoints="dataPoints" />
-    </div>
+    <!-- <HomePageBarChart /> -->
+    <!-- <TimelapseContainer />
+    <HomePageHeatMap /> -->
   </div>
 </template>
   
@@ -29,6 +26,7 @@ const title = ref('CALIFORNIA WILDFIRES');
 
 const currentYear = ref(2015);
 const currentMonth = ref(new Date().getMonth() + 1);
+
 onMounted(() => {
   const storedYear = sessionStorage.getItem('selectedYear');
   const storedMonth = sessionStorage.getItem('selectedMonth');
@@ -36,34 +34,20 @@ onMounted(() => {
     currentYear.value = parseInt(storedYear, 10);
     currentMonth.value = parseInt(storedMonth, 10);
   }
-
+  // console.log(currentYear.value)
 });
 
-const dataPoints = ref([]);
 const isLoading = ref(true);
 const firstLoad = ref(true);
 
-const handleDataUpdate = (updatedData) => {
-  dataPoints.value = updatedData;
-  isLoading.value = false;
-  if (firstLoad.value) {
-    firstLoad.value = false;
-  }
-};
-
-const filteredDataPoints = computed(() => {
-  return dataPoints.value.filter(dp => dp.year === currentYear.value && dp.month === currentMonth.value);
-});
-
 const updateYearMonth = (yearMonth) => {
   const { year, month } = yearMonth;
+  // console.log('update')
   currentYear.value = year;
   currentMonth.value = month;
   sessionStorage.setItem('selectedYear', year);
   sessionStorage.setItem('selectedMonth', month);
 };
-
-
 
 </script>
   
@@ -101,5 +85,4 @@ const updateYearMonth = (yearMonth) => {
   z-index: 1000;
   width: 40%;
 }
-
 </style>
